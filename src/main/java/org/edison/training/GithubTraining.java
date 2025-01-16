abbb95480231464b19aeeae425e5ded45ba8ec33
luisa <luisa@edisoninteractive.com>
2025-01-16 13:33:08 -0700

    added training class

---
+++ src/main/java/org/edison/training/GithubTraining.java
@@ -0,0 +1,11 @@
+package org.edison.training;
+
+public class GithubTraining {
+
+    public static String training = "THIS IS A TRAINING CLASS";
+
+    public static void printSomething(String string){
+
+        System.out.println(training);
+    }
+}
